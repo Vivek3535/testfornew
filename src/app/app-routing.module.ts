ef8de7318006e3756b1aeb4c5510df2f3459e3ba
vivek3535 <vivek@dgstudio.com>
2018-06-01 15:38:22 +0530

    First Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,9 @@
+// import { RouterModule, Routes } from '@angular/router';
+// import { ModuleWithProviders } from '@angular/core/src/metadata/ng_module';
+// import { HomePage } from '../pages/home/home';
+ 
+// export const AppRoutes: Routes = [
+//     { path: 'home', component: HomePage }
+// ];
+ 
+// export const ROUTING: ModuleWithProviders = RouterModule.forRoot(AppRoutes);
---
+++ src/pages/loginPannel/loginPannel.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-loginPannel',
+  templateUrl: 'loginPannel.html'
+})
+export class loginPannel { 
+ 
+  constructor(public navCtrl: NavController) {
+
+  } 
+
+}
